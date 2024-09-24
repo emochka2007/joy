69ec494df0e390f3b78d1005c1029b650fa2a25e
emochka2007 <vrotexwork@gmail.com>
2024-09-24 12:22:56 +0200

    match brackets and inner arrays

--- index.py
+++ index.py
@@ -2,41 +2,46 @@ import re
 def polish_notation(m):
     """polish notation"""
     stack = []
-    inner_arr = []
-    for i, token in enumerate(m):
+    for token in m:
         if isinstance(token,int) or is_number(token):
-            stack.append(int(token))
+            stack.append(token)
         elif isinstance(token, list):
-            inner_arr = []
-            for i in token:
-                inner_arr.append(int(i))
+            stack.append(token)
         else:
-            perform_operation(token, stack, inner_arr) 
+            perform_operation(token, stack) 
     return stack
-def perform_operation(token,stack, inner_arr):
-    if token == "first":
-        stack.append(inner_arr[0])
-    if token == "dup":
+def perform_operation(oper,stack):
+    if oper == "first":
+        stack.append(stack.pop()[0])
+    if oper == "rest":
+        stack_top = stack.pop()
+        del stack_top[0]
+        stack.append(stack_top)
+    if oper == "cons":
+        stack_top = stack.pop()
+        stack_top.insert(0, stack.pop())
+        stack.append(stack_top)
+    if oper == "dup":
         stack.append(stack[-1])
-    if token == "drop":
+    if oper == "drop":
         stack.pop()
-    if token == "swap":
+    if oper == "swap":
         last = stack.pop()
         pre_last = stack.pop()
         stack.append(last)
         stack.append(pre_last)
-    if token == '+':
+    if oper == '+':
         a = stack.pop()
         b = stack.pop()
         stack.append(a+b)
-    if token == '-':
+    if oper == '-':
         a, b = stack.pop(), stack.pop()
         stack.append(b - a)
-    if token == '*':
+    if oper == '*':
         a = stack.pop()
         b = stack.pop()
         stack.append(a*b)
-    if token == '/':
+    if oper == '/':
         a = stack.pop()
         b = stack.pop()
         stack.append(b/a)
@@ -48,32 +53,54 @@ def is_number(text):
     number_regex = r'^[0-9]+$'
     return re.match(number_regex, text) is not None
 
-def app_split(text):
+def tokenizer(text):
     """app_split"""
     splitted = []
-    bracket_open = False
-    inner_array = []
     temp_str = ""
     for i in text:
-       if i != " ":
-           if i == "]":
-               splitted.append(inner_array)
-               bracket_open=False
-           elif bracket_open:
-               inner_array.append(i)
-           elif i == "[":
-               bracket_open=True
-           else: 
-               temp_str = f"{temp_str}{i}" 
-       if i == " " and not bracket_open:
+       if i in ("[", "]"):
+           if temp_str != "":
+               splitted.append(temp_str)
+           splitted.append(i)
+           temp_str = ""
+       elif i != " ":
+           temp_str = f"{temp_str}{i}"
+       else:
            if temp_str != "":
                splitted.append(temp_str)
            temp_str = ""
     splitted.append(temp_str)
 
     return splitted
-# "3 2 swap -" -> [3,2,"swap","-"]
-split_exp = "3 2 swap -"
-first_exp = "[5 2 3] first" #1
-print(polish_notation(app_split(split_exp)))
-print(polish_notation(app_split(first_exp)))
+# # "3 2 swap -" -> [3,2,"swap","-"]
+# split_exp = "3      5 swap -"
+# # first_exp = "[5 2 3] first" #1   
+# first_exp = "[1 2]    3  4 5" #1   
+# print(app_split(first_exp))
+# # print(polish_notation(app_split(first_exp)))
+
+# "[1 2]3 4 5" -> ["43, [","1","2","]","3","4","5"] -> [43,["1","2"],"3","4","5"]
+def match_brackets(splitted):
+    stack = []
+    for i in splitted:
+        if i != "]":
+            if is_number(i):
+                stack.append(int(i))
+            else:
+                stack.append(i)
+        else:
+            matched = []
+            while stack[-1] != "[":
+                matched.insert(0, stack.pop())
+            stack.pop()
+            stack.append(matched)         
+    return stack
+
+print(polish_notation(match_brackets(tokenizer("[1 2 3] first"))))
+print(polish_notation(match_brackets(tokenizer("[1 2 3] rest"))))
+print(polish_notation(match_brackets(tokenizer("42 [1 2 3] cons"))))
+print(polish_notation(match_brackets(tokenizer("[1 2 3] dup first swap rest cons"))))
+# [1 2 3] first -> 1
+# [1 2 3] rest -> [2 3]
+# 42 [1 2 3] cons -> [42 1 2 3]
+# [1 2 3] dup first swap rest cons
